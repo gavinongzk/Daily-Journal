3a3bf5f795dc4a77d4623a3f02b79df887239edf
Gavin Ong <kukudoom@gmail.com>
2022-02-12 20:56:52 +0800

    Commit

--- app.js
+++ app.js
@@ -89,7 +89,13 @@ app.get("/posts/:postId", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+};
+
+
+app.listen(port, function() {
   console.log("Server started.");
 });
 
